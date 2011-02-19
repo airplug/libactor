48c4b9712037fb534ec76592cc79762aa17fbf81
James Harrison Fisher <jameshfisher@gmail.com>
2011-02-19 15:29:54 +0000

    cpplint

--- src/actor.h
+++ src/actor.h
@@ -1,30 +1,29 @@
 /*
-libactor - A C Actor Library
-actor.h
-Copyright (C) 2009 Chris Moos
+  Copyright (C) 2009 Chris Moos
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef ACTOR_H
-#define ACTOR_H
+#ifndef SRC_ACTOR_H_
+#define SRC_ACTOR_H_
 
-#include "types.h"
+#include "./types.h"
 
 enum {
-	ACTOR_MSG_EXITED = 1
+  ACTOR_MSG_EXITED = 1
 };
 
 
@@ -65,4 +64,4 @@ void arelease(void *block);
 void aretain(void *block);
 
 
-#endif
+#endif  // SRC_ACTOR_H_
