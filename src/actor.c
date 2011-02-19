d18ef86838fc39cc6ca9abdf64513ed64ebbe7f4
James Harrison Fisher <jameshfisher@gmail.com>
2011-02-19 16:00:10 +0000

    normal header contents

--- src/actor.c
+++ src/actor.c
@@ -1,7 +1,6 @@
 /*
-  libactor - A C Actor Library
-  actor.c
   Copyright (C) 2009 Chris Moos
+
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
